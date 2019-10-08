afd5eafcfff475df9e0af457d5478214d0ed0dd7
as.lozhkovoy <as.lozhkovoy@tensor.ru>
2019-10-08 16:03:47 +0700

    добавлена обработка типов текста

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import telebot
+import api_key
 
-bot = telebot.TeleBot('726216745:AAFW4QEbfgACybljnePahzixRCfu00eknpg')
+bot = telebot.TeleBot(api_key)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,'Привет, ты написал мне /start')
 
-bot.polling()
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+
+try:
+    bot.polling(none_stop=True, interval=0)
+except Exception:
+    pass
